7d16b5916841a35ff2e85d312a62281f0eede4d9
Mayara Marques <mayaramarquescosta@outlook.com>
2022-07-20 17:36:51 -0300

    Requisito 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
+    const obj = missions.map((el) => (
+      <MissionCard
+        key={ el.name }
+        name={ el.name }
+        year={ el.year }
+        country={ el.country }
+        destination={ el.destination }
+      />
+    ));
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <div>
+          {obj}
+        </div>
       </div>
     );
   }
